3cb4864b2b2a54e02c5da25eae6d93d805110138
Md Aman <aman.md0109@gmail.com>
2025-05-13 14:47:55 +0530

    Hooks

---
+++ WhyUseHooks/src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(5)
+
+ 
+const increaseCounter = ()=>
+{
+  if(count<25)
+  {
+setCount(count+1)
+console.log("Clicked",count);
+  }
+
+  else{
+    let x = document.createElement("div");
+            let t =
+                document.createTextNode("Greater than 25 not possible");
+            x.appendChild(t);
+            document.body.appendChild(x);
+
+  }
+}
+
+const decreaseCounter =() =>
+{
+  if(count>0)
+  {
+setCount(count-1)
+console.log("Clicked",count);
+  }
+
+  else{
+    let x = document.createElement("div");
+            let t =
+                document.createTextNode("Negative Not possible");
+            x.appendChild(t);
+            document.body.appendChild(x);
+  }
+
+}
+
+  return (
+    <>
+      <h1>{count}</h1>
+      <button id
+      onClick={increaseCounter}>Increase Counter</button><br/><br/>
+      <button
+      onClick={decreaseCounter}>Descrease counter</button>
+    </>
+  )
+}
+
+export default App
